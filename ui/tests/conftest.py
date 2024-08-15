83fef21b3aec198293111d1efb5d7005a0765b14
Ekaterina <ekshepa@gmail.com>
2024-08-15 20:40:30 +0300

    fixed conf3

--- ui/tests/conftest.py
+++ ui/tests/conftest.py
@@ -22,6 +22,7 @@ if not os.path.exists(SCREENSHOTS_DIR):
     os.makedirs(SCREENSHOTS_DIR)
 
 
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
